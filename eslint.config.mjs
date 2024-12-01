6c21faa6abb601e9ce18d8cafe28dc90fd6932cc
Mariusz Skomra <skomra.mariusz@gmail.com>
2024-12-01 17:21:03 +0100

    Simplify config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,14 @@ import playwright from "eslint-plugin-playwright";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   playwright.configs["flat/recommended"],
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
 ];
