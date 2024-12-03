9037909586730bcd3c98169aee5f294009060555
Mariusz Skomra <skomra.mariusz@gmail.com>
2024-12-03 11:45:36 +0100

    Fix eslint violations

--- tests/addToCart.spec.ts
+++ tests/addToCart.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, Page } from "@playwright/test"
+import { test, expect } from "@playwright/test"
 import { CartPage } from "../page/CartPage"
 import { CheckoutOveriewPage } from "../page/CheckoutOverviewPage"
 import { InventoryPage } from "../page/InventoryPage"
--- tests/checkoutValidation.spec.ts
+++ tests/checkoutValidation.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, Page } from "@playwright/test"
+import { test, expect } from "@playwright/test"
 import { CheckoutYourInformationPage } from "../page/CheckoutYourInformationPage"
 import { LoginPage } from "../page/LoginPage"
 
